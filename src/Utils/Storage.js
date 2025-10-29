9d620653485d3cb53582a2b177374b1f61a5e63e
Alen bruce <alenbruce1999@gmail.com>
2025-10-29 18:48:43 +0530

    Asyncstorage

---
+++ src/Utils/Storage.js
@@ -0,0 +1,9 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+
+export const storeData=(key, value)=>{AsyncStorage.setItem(key, value)}
+
+export const retriveData=async(key)=>{
+    
+    const data = await AsyncStorage.getItem(key)
+    return data;
+}
