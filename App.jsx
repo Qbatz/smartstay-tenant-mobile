0a72ec4d1e0f10a37c1ead20a8db061abc7fc3ee
Sivasri234 <sivadhanyasri143@gmail.com>
2025-11-11 22:14:54 +0530

    payments

--- App.jsx
+++ App.jsx
@@ -41,6 +41,8 @@ import Agreement from './src/Components/RentalAggreements/Aggreements';
 import SignatureScreen from './src/Components/RentalAggreements/SignatureScreen'
 import ReceiptPdfView from './src/Components/ReceiptPdfviewer';
 import AgreementViewScreen from './src/Components/RentalAggreements/AggreementView';
+import Payment from './src/Components/Payment';
+import InvoiceDesign from './src/Components/Payments/BillPDF';
 
 function App() {
 
@@ -119,7 +121,7 @@ function AppContent() {
 
     <View style={styles.container}>
     <NavigationContainer >
-      <Navigation.Navigator screenOptions={{headerShown:false}} initialRouteName='Dashboard'>
+      <Navigation.Navigator screenOptions={{headerShown:false}} initialRouteName='InvoiceDesign'>
 
         <Navigation.Screen name="HostelList" component={HostelList} />
         <Navigation.Screen name='Dashboard' component={Dashboard}/>  
@@ -140,6 +142,8 @@ function AppContent() {
         <Navigation.Screen name="ReceiptPdfView" component={ReceiptPdfView} />
         <Navigation.Screen name="AgreementViewScreen" component={AgreementViewScreen} />
         <Navigation.Screen name="SuccessModal" component={SuccessModal} />
+        <Navigation.Screen name="Payment" component={Payment} />
+         <Navigation.Screen name="InvoiceDesign" component={InvoiceDesign} />
 
       </Navigation.Navigator>
     </NavigationContainer>
